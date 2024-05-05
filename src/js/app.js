17430fed0e5b4448fd22b68eb9772c657ecc5f10
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2024-05-05 12:19:08 -0600

    AGREGAR, QUITAR servicio

--- src/js/app.js
+++ src/js/app.js
@@ -171,11 +171,19 @@ function seleccionarServicio(servicio) {
     // console.log("Desde seleccionar", servicio);
     const { servicios } = cita;
 
-    // ... = toma una copia de servicios y le agrega servicio
-    cita.servicios = [...servicios, servicio];
-
+    // Identificar el elemento al que se le da CLICK
     const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
-    divServicio.classList.toggle('seleccionado');
-
+    
+    // Comprobar si un servicio ya fue agregado
+    if ( servicios.some( agregado => agregado.id === id ) ) {
+        // Eliminarlo
+        cita.servicios = servicios.filter( agregado => agregado.id !== id );
+        divServicio.classList.remove('seleccionado');
+    } else {
+        // ... = toma una copia de servicios y le agrega servicio
+        // Agregarlo
+        cita.servicios= [...servicios, servicio];
+        divServicio.classList.add('seleccionado');
+    }
     console.log(cita);
 }
