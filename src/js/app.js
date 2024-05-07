85da31998bd69c532687c1e7bd2e5dc34fc2f9bd
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2024-05-07 00:26:32 -0600

    Añadir BOTON para reservar citas

--- src/js/app.js
+++ src/js/app.js
@@ -324,7 +324,17 @@ function mostrarResumen() {
     const horaCita = document.createElement('P');
     horaCita.innerHTML = `<span>Hora:</span> ${hora} horas`;
 
+    // Boton para crear una cita
+    const botonReservar = document.createElement('BUTTON');
+    botonReservar.classList.add('boton');
+    botonReservar.textContent = 'Reservar Cita';
+    botonReservar.onclick = reservarCita;
+
     resumen.appendChild(nombreCliente);
     resumen.appendChild(fechaCita);
     resumen.appendChild(horaCita);
+}
+
+function reservarCita() {
+    
 }
