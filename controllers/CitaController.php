7cfd5db08390fe4157b8c01e5d8c188ddd3c7411
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2024-05-11 10:58:33 -0600

    Proteger la RUTA de CITA

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -7,6 +7,8 @@
         public static function index(Router $router) {
             // session_start();
 
+            isAuth();
+
             $router->render('cita/index', [
                 'nombre' => $_SESSION['nombre'],
                 'id' => $_SESSION['id'],
