2adac24d697c1d92a5037a61f68eac2982fc7fd7
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2024-05-15 22:30:47 -0600

    CSS a los servicios

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -10,6 +10,16 @@
         <li>
             <p>Nombre: <span> <?php echo $servicio->nombre; ?> </span> </p>
             <p>Precio: <span> $<?php echo $servicio->precio; ?> </span> </p>
+
+            <div class="acciones">
+                <a href="/servicios/actualizar?id=<?php echo $servicio->id; ?>" class="boton">Actualizar</a>
+
+                <form action="/servicios/eliminar" class="" method="POST">
+                    <input type="hidden" name="id" id="id" value="<?php echo $servicio->id;?>">
+
+                    <input type="submit" value="Eliminar" class="boton-eliminar">
+                </form>
+            </div>
         </li>
     <?php } ?>
 </ul>
