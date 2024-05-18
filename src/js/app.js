19be0c25ed1a3e266d91e08f42e7b7e4031a32a4
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2024-05-17 23:23:02 -0600

    Subir proyecto a GIT

--- src/js/app.js
+++ src/js/app.js
@@ -135,7 +135,7 @@ function paginaSiguiente() {
 
 async function consultarAPI() {
     try {
-        const url = `/api/servicios`;
+        const url = '/api/servicios';
         const resultado = await fetch(url);
         const servicios = await resultado.json();
 
@@ -361,7 +361,8 @@ async function reservarCita() {
     try {
         // Peticiones hacia la API
         // const url = 'http://localhost:3000/api/citas';
-        const url = `${location.origin}/api/servicios`;
+        // const url = `${location.origin}/api/servicios`;
+        const url = '/api/servicios';
 
         const respuesta = await fetch(url, {
             method: 'POST',
